9ad20e4759c55094cc71aed01c78bb637505b947
Lucile Mt <mt.lucile@hotmail.fr>
2022-07-08 09:18:30 +0200

    derniere maj

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <React.Fragment>
         <div className='text-center footerbg'>
-            <div className='text-center p-3'>
+            <div className='text-center p-3 mt-5'>
                 &copy; Lucile Monnet
             </div>
 
